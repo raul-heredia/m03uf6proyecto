6247eea247ceb87aa35c86e29a7803e410a9676c
Raúl Heredia <raul.heredia01@outlook.com>
2022-03-26 15:14:00 +0100

    first commit

---
+++ database.sql
@@ -0,0 +1,60 @@
+USE m03uf6proyecto;
+CREATE TABLE clientes(
+    dni VARCHAR(9) NOT NULL PRIMARY KEY,
+    nombreCompleto VARCHAR(100) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    telefono VARCHAR(9) NOT NULL,
+    direccion VARCHAR(30) NOT NULL,
+    ciudad VARCHAR(30) NOT NULL,
+    pais VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    puntosCarnet INT NOT NULL
+);
+CREATE TABLE coches(
+    matricula VARCHAR(7) NOT NULL PRIMARY KEY,
+    numeroBastidor VARCHAR(17) NOT NULL,
+    marca VARCHAR(25) NOT NULL,
+    modelo VARCHAR(25) NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    numeroPlazas INT NOT NULL,
+    numeroPuertas INT NOT NULL,
+    grandariaMaletero INT NOT NULL,
+    combustible ENUM('Gasolina','Diésel') NOT NULL
+);
+CREATE TABLE alquilerCoches(
+    matricula VARCHAR(7),
+    dni VARCHAR (9),
+    fechaInicio DATE,
+    fechaFinal DATE,
+    precioPorDia DOUBLE,
+    isRetornDipositPle TINYINT,
+    tipoSeguro ENUM('Franquícia','Sin Franquícia'),
+    PRIMARY KEY (matricula, dni),
+    FOREIGN KEY (matricula) REFERENCES coches(matricula) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (dni) REFERENCES clientes(dni) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE mecanicos(
+    dni VARCHAR(9) NOT NULL PRIMARY KEY,
+    nombreCompleto VARCHAR(100) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    telefono VARCHAR(9) NOT NULL,
+    direccion VARCHAR(30) NOT NULL,
+    ciudad VARCHAR(30) NOT NULL,
+    pais VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    puntosCarnet INT NOT NULL,
+    salario DOUBLE NOT NULL,
+    numSS INT NOT NULL,
+    titulacion VARCHAR(30) NOT NULL,
+    fechaContratacion DATE NOT NULL
+);
+
+CREATE TABLE mantenimientoCoches(
+    matricula VARCHAR(7),
+    dni VARCHAR (9),
+    fechaInMantenimiento DATE,
+    fechaFiMantenimiento DATE,
+    PRIMARY KEY (matricula, dni),
+    FOREIGN KEY (matricula) REFERENCES coches(matricula) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (dni) REFERENCES mecanicos(dni) ON DELETE CASCADE ON UPDATE CASCADE
+);
---
+++ inserts.sql
@@ -0,0 +1,5 @@
+# Clientes
+insert into clientes (dni,nombreCompleto,fechaNacimiento,telefono,direccion,ciudad,pais,email,puntosCarnet) values("21761046X","Raúl Heredia Maza",2001-10-02,"629623372","C/ Mallorca 654","Barcelona","España","raul.heredia@hotmail.com",12);
+insert into clientes (dni,nombreCompleto,fechaNacimiento,telefono,direccion,ciudad,pais,email,puntosCarnet) values("21098714P","Marc Carbonell Sariola",1999-03-28,"665135129","C/ Pixapins 33","Barcelona","España","marc.carbonell@hotmail.com",8);
+# Coches
+insert into coches (matricula,numeroBastidor,marca,modelo,color,numeroPlazas,numeroPuertas,grandariaMaletero,combustible) values("3333BCM ","F7RCPWEWJ4PVYW96T","Chevrolet","Corvette Stingray 67","Rojo","2","3",250,"Gasolina");
